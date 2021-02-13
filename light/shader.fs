0f64b8f1537f9ed1e7d9e74dd48b9a6d869e1d82
KatherineFanfan <fanwenjing9362@gmail.com>
2021-02-12 17:35:30 -0800

    Gouraud shading

--- light/shader.fs
+++ light/shader.fs
@@ -1,32 +1,11 @@
 #version 330 core
-in vec3 Normal;
-in vec3 FragPos;
-in vec3 LightPos;
+in vec3 LightingColor;
 
 out vec4 FragColor;
   
 uniform vec3 objectColor;
-uniform vec3 lightColor;
 
 void main()
 {
-    // ambient
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-    
-    // diffuse
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(LightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-    
-    // specular
-    float specularStrength = 0.5;
-    vec3 viewDir = normalize(-FragPos);  // viewPos = (0.0, 0.0, 0.0)
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
-    
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(LightingColor * objectColor, 1.0);
 }
